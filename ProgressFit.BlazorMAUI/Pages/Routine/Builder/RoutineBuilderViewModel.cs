542f9f86987f10f3a3442d14acefb88df3210898
Evan Bonfiglio <evanbonfiglio@gmail.com>
2022-11-28 17:35:17 -0500

    Routine view model work

--- ProgressFit.BlazorMAUI/Pages/Routine/Builder/RoutineBuilderViewModel.cs
+++ ProgressFit.BlazorMAUI/Pages/Routine/Builder/RoutineBuilderViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using ProgressFit.MauiAppLib.Endpoints.Routine.Contracts;
+using ProgressFit.Shared.Models.Requests;
+using ProgressFit.Shared.Models.Responses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,36 @@ using System.Threading.Tasks;
 
 namespace ProgressFit.BlazorMAUI.Pages.Routine.Builder
 {
-    internal class RoutineBuilderViewModel
+     public class RoutineBuilderViewModel : ComponentBase
     {
+        [Inject]
+        public IRoutineEndpoint RoutineEndpoint { get; set; }
+
+        public RoutineRequest Routine { get; set; } = new();
+
+        [Parameter]
+        public Guid? RoutineId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            //get routine if ID provided...
+            //Guid.TryParse(RoutineId, out var routineId);
+
+            if (RoutineId is not null)
+            {
+                //Routine = await ExpenseService.GetExpenseById(int.Parse(ExpenseId));
+            }
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            //if Id save, if no ID create...
+            await create();
+        }
+
+        private async Task<RoutineResponse> create()
+        {
+            return await RoutineEndpoint.Create(Routine);
+        }
     }
 }
